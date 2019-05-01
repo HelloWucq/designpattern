2a67d5bcea1d3fcb4eae052ac439edf9c798a79b
wuchenqi <2455703859@qq.com>
2019-05-01 20:34:29 +0800

    工厂模式

--- normalfactory/MailSender.java
+++
@@ -1,6 +0,0 @@
-public class MailSender implements Sender {
-    @Override
-    public void Send() {
-        System.out.println("this is mail-sender!");
-    }
-}
--- normalfactory/SmsSender.java
+++
@@ -1,6 +0,0 @@
-public class SmsSender implements Sender {
-    @Override
-    public void Send() {
-        System.out.println("this is sms sender!");
-    }
-}
